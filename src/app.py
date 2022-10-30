a1f0efcf9d095d9255c4b751e12a3bc0c1938208
M4kI9H7 <03bbosong@gmail.com>
2022-10-30 11:35:23 +0900

    app update

--- src/app.py
+++ src/app.py
@@ -16,10 +16,9 @@ def main():
     screen = pygame.display.set_mode((_SCREEN_WIDTH, _SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
-    player = Player.Player(200, 200) #플레이어 생성
-    puck = Hockey_Puck.Hockey_Puck(200,400) #퍽 생성
-    
-    print(puck.rect.center)
+    player = Player.Player(_SCREEN_WIDTH / 2 - 32, _SCREEN_HEIGHT-64) #플레이어 생성
+    puck = Hockey_Puck.Hockey_Puck(_SCREEN_WIDTH / 2 - 32, _SCREEN_HEIGHT / 2 - 32) #퍽 생성
+
 
     while True:
         clock.tick(120) #프레임 120FPS 로 설정
@@ -40,44 +39,55 @@ def main():
         
         if key_event[pygame.K_DOWN]:
             player.pos[1] += 1 * df
-            
+                        
         screen.fill((0,0,0)) # 화면을 검정색으로 채움
         
         player.update() #플레이어와 퍽에 있는 update() 함수 호출, 각각의 Rect라는 Collider 를 각자의 위치에 맞게 업데이트함
         puck.update()
         
         
+        # 하키 퍽이 화면 가장자리에서 튕기게 하는 로직
+        if (puck.pos[0] < 0):
+            if puck.move_vec[0] < 0: #하키 퍽이 화면 가장자리에서 무한정 이동방향을 반전시키지 않게 확인
+                puck.move_vec[0] *= -1
+            
+        if (puck.pos[1] < 0):
+            if puck.move_vec[1] < 0: #하키 퍽이 화면 가장자리에서 무한정 이동방향을 반전시키지 않게 확인
+                puck.move_vec[1] *= -1
+            
+        if (puck.pos[0] + 64 > _SCREEN_WIDTH):
+            if puck.move_vec[0] > 0: #하키 퍽이 화면 가장자리에서 무한정 이동방향을 반전시키지 않게 확인
+                puck.move_vec[0] *= -1
+            
+        if (puck.pos[1] + 64 > _SCREEN_HEIGHT):
+            if puck.move_vec[1] > 0: #하키 퍽이 화면 가장자리에서 무한정 이동방향을 반전시키지 않게 확인
+                puck.move_vec[1] *= -1
+                
         
         
+        #플레이어와 하키 퍽이 충돌했을 때 로직
         if (pygame.sprite.collide_mask(player, puck)):
             fDistance = math.sqrt(math.pow((player.pos[0] - puck.pos[0]), 2) + math.pow((player.pos[1] - puck.pos[1]), 2))
             puck.move_vec = [puck.pos[0] - player.pos[0], puck.pos[1] - player.pos[1]]
             puck.move_vec[0] = puck.move_vec[0] / fDistance
             puck.move_vec[1] = puck.move_vec[1] / fDistance
             
-        if (puck.pos[0] < 0):
-            if puck.move_vec[0] < 0:
-                puck.move_vec[0] *= -1
-            
-        if (puck.pos[1] < 0):
-            if puck.move_vec[1] < 0:
-                puck.move_vec[1] *= -1
-            
-        if (puck.pos[0] + 64 > _SCREEN_WIDTH):
-            if puck.move_vec[0] > 0:
-                puck.move_vec[0] *= -1
-            
-        if (puck.pos[1] + 64 > _SCREEN_HEIGHT):
-            if puck.move_vec[1] > 0:
-                puck.move_vec[1] *= -1
+        #2번째 플레이어와 하키 퍽이 충돌했을 때를 구현해야 함
+        
+        #하키 퍽이 각 진영의 골에 도달했을 때 스코어를 올리고 게임 판을 초기화하는 로직을 구현해야 함
+        
+        #플레이어1, 플레이어2가 화면 가장자리를 벗어나지 못하게 하는 것을 구현해야 함
+        
+        #플레이어1, 플레이어2가 각각의 진영을 벗어나지 못하게 하는 것을 구현해야 함
 
+
+        #하키 퍽의 x,y 위치를 이동방향(x,y) * 속도(1) * 프레임 델타의 값으로 움직임
         puck.move((puck.move_vec[0] * puck.speed * df ), (puck.move_vec[1] * puck.speed * df ))
         
         screen.blit(puck.image, (puck.pos[0], puck.pos[1]))
         screen.blit(player.image, (player.pos[0], player.pos[1]))
         
         pygame.display.update()
-        
 
 if (__name__ == '__main__'):
     main()
